2a5d1260fd417aaf6f6e4218555fb9145dae26b2
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-21 01:44:24 +0530

    react-router-dom configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
-import Navbar from "./components/shared/Navbar"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from "../src/components/auth/Login";
+import Signup from "../src/components/auth/Signup";
+import Home from "../src/components/Home";
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signup',
+    element: <Signup/>
+  },
+])
 
 function App() {
 
 
   return (
     <>
-      <Navbar />
+      <RouterProvider router={appRouter} />
     </>
   )
 }
