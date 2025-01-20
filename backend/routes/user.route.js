482602643f4005647958499b5b7878eef80e17ff
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-21 03:10:20 +0530

    Added multer configuration

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { login, logout, register, updateProfile } from '../controllers/user.controller.js';
 import isAuthenticated from '../middleware/auth.middleware.js';
+import { singleUpload } from "../middleware/multer.middleware.js";
 
 const router = express.Router();
 
-router.route('/register').post(register);
+router.route('/register').post(singleUpload, register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
-router.route('/profile/update').put(isAuthenticated,updateProfile);
+router.route('/profile/update').post(isAuthenticated,updateProfile);
 
 export default router;
