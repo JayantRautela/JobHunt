d92336a6a5e376c1254119b00865126df8cebe67
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-21 02:44:32 +0530

    Added signup and login page

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,11 +1,120 @@
-import React from 'react'
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 const Signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: ""
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({...input, [e.target.name]:e.target.value});
+  }
+
+  const changeFileHandler = (e) => {
+    setInput({...input, file: e.target.files?.[0]});
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  }
+
   return (
     <div>
-      
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form onSubmit={submitHandler} className="w-1/2 border border-gray-200 rounded-md p-4 my-10">
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input 
+              type="text" 
+              placeholder="jayant" 
+              value={input.fullname} 
+              name='fullname'
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input 
+              type="email" 
+              placeholder="jayant@gmail.com" 
+              value={input.email} 
+              name='email'
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Phone Number</Label>
+            <Input 
+              type="tel" 
+              placeholder="8888899999" 
+              value={input.phoneNumber} 
+              name='phoneNumber'
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input 
+              type="password"  
+              value={input.password} 
+              name='password'
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="Student"
+                  checked={input.role === 'Student'}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="option-one">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+              <Input
+                  type="radio"
+                  name="role"
+                  value="Recruiter"
+                  checked={input.role === 'Recruiter'}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="option-two">Recruiter</Label>
+              </div>
+            </RadioGroup>
+            <div className="flex items-center gap-2">
+              <Label>Profile</Label>
+              <Input
+                accept="image/*"
+                type="file"
+                className="cursor-pointer"
+                onChange={changeFileHandler}
+              />
+            </div>
+          </div>
+          <Button tpye="submit" className='w-full my-4'>Signup</Button>
+          <span className="text-sm">Already Have an Account? <Link to="/login">Login</Link></span>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
