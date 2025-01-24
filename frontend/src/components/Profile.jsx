901c107c91cba62e7e857949a617011a88e36761
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-24 13:09:19 +0530

    added dynamic nature to profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -7,10 +7,10 @@ import { Label } from './ui/label';
 import AppliedJobTable from './AppliedJobTable';
 import { useState } from 'react';
 import UpdateProfileDialog from './UpdateProfileDialog'
-
-const skills = ['html', 'css', 'reactjs', 'js', 'nodejs'];
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
+  const { user } = useSelector(store => store.auth);
   const [open, setOpen] = useState(false);
   const isResume = true;
   return (
@@ -23,8 +23,8 @@ const Profile = () => {
             <AvatarImage src="" alt="user profile photo"></AvatarImage>
           </Avatar>
           <div>
-            <h1 className='font-medium text-xl'>Full Name</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <h1 className='font-medium text-xl'>{user.fullname}</h1>
+            <p>{user.bio}</p>
           </div>
         </div>
         <Button onClick={() => setOpen(true)} className='text-right' variant='outline'><Pen /></Button>
@@ -32,18 +32,18 @@ const Profile = () => {
         <div>
           <div className="flex items-center gap-3 my-2">
             <Mail />
-            <span>jayantrautela2005@gmail.com</span>
+            <span>{user.email}</span>
           </div>
           <div className="flex items-center gap-3">
             <Contact />
-            <span>7985626683</span>
+            <span>{user.phoneNumber}</span>
           </div>
         </div>
         <div className='my-5'>
           <h1>Skills</h1>
           <div className="flex items-center gap-1">
             {
-              skills.length !== 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
+              user.profile?.skills.length !== 0 ? user.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
             }
           </div>
         </div>
