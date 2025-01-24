b5069a2e92bba65886cc16e7b16861c5185126ce
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-24 16:08:58 +0530

    user side completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -7,6 +7,9 @@ import cloudinary from '../utils/cloudinary.js';
 export const register = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, password, role } = req.body;
+        const file = req.file;
+        const fileUri = getDataUri();
+        const cloudRes = await cloudinary.uploader.upload(fileUri.content);
 
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
@@ -30,7 +33,10 @@ export const register = async (req, res) => {
             email,
             phoneNumber,
             password: hashedPassword,
-            role
+            role,
+            profile: {
+                profilePhoto: cloudRes.secure_url,
+            }
         });
 
         return res.status(201).json({
