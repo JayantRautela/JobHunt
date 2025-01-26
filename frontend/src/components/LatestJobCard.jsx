53ba522a5224dde6788ef926ac1d64489273a32a
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-26 18:42:58 +0530

    Added custom hook for jobs

--- frontend/src/components/LatestJobCard.jsx
+++ frontend/src/components/LatestJobCard.jsx
@@ -1,23 +1,36 @@
 import { Badge } from "./ui/badge";
+import PropTypes from 'prop-types';
 
-const LatestJobCard = () => {
+const LatestJobCard = ({ job }) => {
   return (
     <div className="border border-gray-300 rounded-lg p-5 shadow-xl bg-white cursor-pointer">
       <div>
-        <h1 className="font-medium text-lg">Company Name</h1>
+        <h1 className="font-medium text-lg">{job?.company?.name}</h1>
         <p className="text-sm text-gray-500">India</p>
       </div>
       <div>
-        <h1 className="font-bold text-lg my-2">Job Title</h1>
-        <p className="text-sm text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa.</p>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
+        <p className="text-sm text-gray-600">{job?.description}</p>
       </div>
       <div className="flex items-center gap-2 mt-4">
-        <Badge className={'text-blue-700 font-bold'} variant={'ghost'}>12 Position</Badge>
-        <Badge className={'text-[#f83002] font-bold ml-2'} variant={'ghost'}>Part-Time</Badge>
-        <Badge className={'text-[#7209b7] font-bold ml-2'} variant={'ghost'}>13 Lpa</Badge>
+        <Badge className={'text-blue-700 font-bold'} variant={'ghost'}>{job?.position}</Badge>
+        <Badge className={'text-[#f83002] font-bold ml-2'} variant={'ghost'}>{job?.jobType}</Badge>
+        <Badge className={'text-[#7209b7] font-bold ml-2'} variant={'ghost'}>{job?.salary} Lpa</Badge>
       </div>
     </div>
   )
 }
+LatestJobCard.propTypes = {
+  job: PropTypes.shape({
+    company: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+    title: PropTypes.string,
+    description: PropTypes.string,
+    position: PropTypes.string,
+    jobType: PropTypes.string,
+    salary: PropTypes.number,
+  }).isRequired,
+};
 
-export default LatestJobCard
+export default LatestJobCard;
---
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+import { JOB_API_END_POINT } from '../utils/constant';
+import { useDispatch } from 'react-redux';
+import { setAllJobs } from '../redux/jobSlice'
+
+const useGetAllJobs = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const fetchAllJobs = async () => {
+            try {
+                const response = await axios.get(`${JOB_API_END_POINT}/get`,
+                    {
+                        withCredentials: true
+                    }
+                );
+                if (response.data.success) {
+                    dispatch(setAllJobs(response.data.jobs))
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchAllJobs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+}
+
+export default useGetAllJobs
