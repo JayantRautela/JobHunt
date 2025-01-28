b5b74f15a80566c06ee278bd61829b96ce22ea05
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-28 22:23:45 +0530

    Added companies table

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Navbar from "../shared/Navbar";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import CompaniesTable from "./ComapniesTable";
 
 const Companies = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div className="max-w-6xl mx-auto my-10">
+        <div className="flex items-center justify-between">
+          <Input
+            className='w-fit'
+            placeholder='FIlter by name'
+          />
+          <Button>New Company</Button>
+        </div>
+        <CompaniesTable />
+      </div>
     </div>
   )
 }
