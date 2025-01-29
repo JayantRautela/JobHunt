1b8fe79134e144234911fbbd2eb236cf02947ebc
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-01-29 19:13:19 +0530

    added useGetCompanyById hook

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -9,9 +9,11 @@ import { COMPANY_API_END_POINT } from "@/utils/constant";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { useSelector } from "react-redux";
+import useGetCompanyById from "@/hooks/useGetCompanyById";
 
 const CompanySetup = () => {
   const params = useParams();
+  useGetCompanyById(params.id);
   const [input, setInput] = useState({
     name: "",
     description: "",
